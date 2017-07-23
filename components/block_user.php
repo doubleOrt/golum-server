33df3583ff3d275ea36019034856b1a7b7810355
Taurus <fantasybl8@gmail.com>
2017-07-23 22:18:17 +0300

    The PHP file used for blocking users is now secure (uses prepared statements).

--- components/block_user.php
+++ components/block_user.php
@@ -6,23 +6,26 @@ include_once "common_requires.php";
 
 
 if(isset($_GET["user_id"]) && filter_var($_GET["user_id"], FILTER_VALIDATE_INT) !== false) {
-$already_blocked = $con->query("select id from blocked_users where user_ids = '".$_SESSION["user_id"]."-".htmlspecialchars($_GET["user_id"], ENT_QUOTES, "utf-8")."'")->fetch();	
+$already_blocked_prepared = $con->prepare("select id from blocked_users where user_ids = concat(:base_user_id, '-', :user_id)");
+$already_blocked_prepared->execute([":base_user_id" => $_SESSION["user_id"], ":user_id" => $_GET["user_id"]]);
+$already_blocked = $already_blocked_prepared->fetch();	
 if($already_blocked[0] == "") {
-if($con->exec("insert into blocked_users (user_ids,time) values('". $_SESSION["user_id"] . "-" . htmlspecialchars($_GET["user_id"], ENT_QUOTES, "utf-8")."',".time().")")) {
-$con->exec("delete from contacts where contact_of = ".$_SESSION["user_id"]." and contact = ". htmlspecialchars($_GET["user_id"], ENT_QUOTES, "utf-8"));	
-$con->exec("delete from contacts where contact = ".$_SESSION["user_id"]." and contact_of = ". htmlspecialchars($_GET["user_id"], ENT_QUOTES, "utf-8"));
+if($con->prepare("insert into blocked_users (user_ids,time) values(concat(:base_user_id, '-', :user_id),:time)")->execute([":base_user_id" => $_SESSION["user_id"], ":user_id" => $_GET["user_id"], ":time" => time()])) {
+$con->prepare("delete from contacts where contact_of = :base_user_id and contact = :user_id")->execute([":base_user_id" => $_SESSION["user_id"], ":user_id" => $_GET["user_id"]]);	
+$con->prepare("delete from contacts where contact = :base_user_id and contact_of = :user_id")->execute([":base_user_id" => $_SESSION["user_id"], ":user_id" => $_GET["user_id"]]);	
 /* nullify the "x is now following you" button inserted previously, just in case the user starts following someone and then immediately unfollows them, 
 else the receiver would be confused */
-$con->exec("delete from notifications where notification_from = ". $_SESSION["user_id"] ." and notification_to = ". $_GET["user_id"] ." and type = 6");
+$con->prepare("delete from notifications where notification_from = :base_user_id and notification_to = :user_id and type = 6")->execute([":base_user_id" => $_SESSION["user_id"], ":user_id" => $_GET["user_id"]]);
 echo "0";
 }
 }
-else if($con->exec("delete from blocked_users where user_ids = '".$_SESSION["user_id"]."-".htmlspecialchars($_GET["user_id"], ENT_QUOTES, "utf-8")."'")){	
+else if($con->prepare("delete from blocked_users where user_ids = concat(:base_user_id, '-', :user_id)")->execute([":base_user_id" => $_SESSION["user_id"], ":user_id" => $_GET["user_id"]])){	
 echo "1";	
 }
 }
 
 
+unset($con);
 
 
 ?>
